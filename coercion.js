7f97c94da4805dd9f7d146b8ba86360a6efc9283
husnain585 <husnain7t9@gmail.com>
2025-01-01 12:13:38 +0500

    coercion topic

---
+++ coercion.js
@@ -0,0 +1,19 @@
+let score  = 200;
+console.log(score);
+console.log(typeof score);
+
+let newScore =  Number(score);
+console.log(newScore);
+console.log(typeof newScore);
+let stringScore = String(score);
+console.log(stringScore);
+console.log(typeof stringScore);
+let booleanScore = Boolean(score);
+console.log(booleanScore);
+console.log(typeof booleanScore);
+let score1 = 0;
+let booleanScore1 = Boolean(score1);
+console.log(booleanScore1);
+console.log(typeof booleanScore1);
+
+console.log("">=1);
