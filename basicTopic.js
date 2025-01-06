4c6c649b5dedb16004563996f06b3631f29c37ed
husnain585 <husnain7t9@gmail.com>
2025-01-07 00:45:15 +0500

    changing in basicTopicJs file

--- basicTopic.js
+++ basicTopic.js
@@ -28,19 +28,21 @@ function greet() {
 var sayHello = function() {
     console.log("Hello!");
 };
+console.log("**********************");
 
 // Primitive dataTypes
 // In Js there are seven Primitive dataTypes which are
 // String, Symbol, Number, Null, Boolean, BigIn, undefined 
 // also here is another datatype is NAN => NotANumber 
 console.log("Example of NAN", 1-"a");
+console.log("**********************");
 
 // How are primitive values compared in JavaScript?
 // Comparing with Object.is()
 console.log(Object.is(0, -0));   // false, because +0 and -0 are considered different
 console.log(Object.is(NaN, NaN)); // true, because NaN is equal to NaN
 console.log(Object.is(5, 5));     // true, both value and type are the same
-
+console.log("**********************");
 // Operator Precedence
 // is k matlb tarje dena ek operator pr kisi dosry operator ko 
 // AND (&&) has a higher precedence than OR (||).
@@ -55,4 +57,53 @@ console.log(A || B && C);  // `b && c` is evaluated first, then `a || (b && c)`
 // Short Circuiting 
 // Covered anytime test this topic
 
-// 
+// Template String
+// Basic example
+let name = 'Husnain';
+let age = 22;
+let firstName = "Husnain";
+let lastName = "Raza";
+let variable = "is";
+let method = "string";
+let usage = "using";
+let num1 = 5;
+let num2 = 3;
+let greeting = `Hello, my name is ${name} and I am ${age} years old.`;
+console.log(greeting); 
+console.log(`Hello, my name is ${firstName} ${lastName} and I am ${age} years old.`);
+let mulStr = `
+this ${variable} a multiline
+${method} example
+${usage} template String
+`;
+console.log(`substraction, ${num1 - num2}`);
+console.log(mulStr);
+console.log("**********************");
+
+// Ternory Operator
+//   Q1  // Basic Ternary Operation
+const umr = (age > 18 ? "you are Adult":"you are Minnor");
+console.log(umr);
+// Q1 //  Nested Ternary Operation
+let score = 102;
+let passingScore = 10;
+let result = (score === 100) ?  "perfect" :
+             (score >= passingScore) ? "pass" : "fail";
+console.log(result);
+let a1 = 0;
+let a2 = 1;
+let checkPos = (a1 > 0) ? "a1 is  positive" :
+                (a2 > 0) ? "a2 is positive" :
+                (a1 > 0 && a2 > 0) ? "both are positive": "Neiter is positive";
+console.log(checkPos);
+
+// let dayOfWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+let dayOfWeek = "Tuesday";
+let checkDay = (dayOfWeek == "Monday") ? "start of Week" : 
+                (dayOfWeek == "Friday") ? "end of week" : 
+                (dayOfWeek == "Saturday" || dayOfWeek == "Sunday") ? "weekend": "Midweek";
+console.log(checkDay);
+
+let arr = [1,2,3,4,5,6];
+let checkArr = (arr.length > 5) ? "array is larger": " array is small";
+console.log(checkArr);
