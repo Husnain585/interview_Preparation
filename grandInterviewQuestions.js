512c9aad3810c127825d99e688751a80a80340be
husnain585 <husnain7t9@gmail.com>
2025-01-14 23:47:52 +0500

    modify grandQuiz file

---
+++ grandInterviewQuestions.js
@@ -0,0 +1,9 @@
+// oop 
+// oop is object oriented programming that based on object 
+// encapsulation 
+// abstraction
+// polymorphism 
+// inheritance
+// class and object 
+// compostion
+// interface 
