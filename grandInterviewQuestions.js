c76d2f381b81810ae800d7f71f8ef78205056cd8
husnain585 <husnain7t9@gmail.com>
2025-01-15 23:15:03 +0500

    Add DataStructure File for Interview Preparation

--- grandInterviewQuestions.js
+++
@@ -1,9 +0,0 @@
-// oop 
-// oop is object oriented programming that based on object 
-// encapsulation 
-// abstraction
-// polymorphism 
-// inheritance
-// class and object 
-// compostion
-// interface 
---
+++ singleSolutionPreparation.js
@@ -0,0 +1,47 @@
+//  ********* DATA STRUCTURE **********
+
+// Data structure => is a way to arrange, orgainze and manage data so it can be used efficiently. 
+
+// Why DataStructure => #Organize, #Improve Performance, #Reuseability and Scalability, #Data Handling
+
+// Classification of DataStructure => { Linean - DataStructure => #Array, #Linkedlist, #Stack, #Queue, 
+                                    //  Non-Linear - DataStructure =>  #Trees, #Graph, #HashTable
+
+// Array's => A collection of multiple elements of same data type which are store in homogeneous memory location 
+
+// Linked LIst => A collection of Nodes, where each node consist of two parts, one part has the data and second part
+// has the address which refer to next node in the linkedlist 
+
+// Stack => A stack is a linear data structure that follows the Last In, First Out (LIFO) principle, 
+// where elements are added and removed from the same end called the top. (push, pop, peek)
+
+// Queue => A Queue is a linear data structure that follows the First In, First out (FIFO) principle,
+// where elements are added and removed from the same end called the top. (enqueue, dequeue, peek)
+
+// Hash Table => A Hash Table is a data structure designed to be fast to work.
+// ${`The reason Hash Tables are sometimes preferred instead of arrays or linked lists is because searching for, adding, and deleting data can be done really quickly, even for large amounts of data.`}
+
+// Tree => A Non-linear data sturcture where data is representing in the form of Nodes and attach with edges,
+//             Mainly Root Node represent parent and leave node represent to child which create a hierarchical.
+
+// Graph  => A Non-Linear data structure which consist on set of node connected with the help of edges which can directed or undirected.
+
+//                                                Cover Basic DataStructure
+
+//      ********** Array based Algorithms *************
+
+// Bubble sort => Bubble Sort is an algorithm that sorts an array from the lowest value to the highest value.
+// it' {Time Complexity N(Square)} [9, 12, 8, 11, 7] => [9, 8, 11, 7, 12] => [8, 9, 7,11, 12] => [8, 7, 9, 11, 12].
+
+// Seleciton Sort => The Selection Sort algorithm finds the lowest value in an array and moves it to the front of the array.
+// it' {Time Complexity N(Square)} [6, 1, 3, 1, 5] => [1, 6, 3, 1, 5] => [1, 1, 6, 3 ,5] => [1, 1, 3, 6, 5] => [1, 1, 3, 5, 6]
+
+// Insertion Sort => Insertion sort is a simple sorting algorithm that builds a sorted array one element at a time by repeatedly taking the next element from the unsorted portion and inserting it into its correct position within the sorted portion. {TimeComplexity N(Square)}
+
+// Quick Sort => bookShelf Example of Quick sort time complexity is worst case N(Square) and average case N(n logn)
+
+// Merge Sort => Follow divid and Conquer method to achieve desired result {TimeComplexity N(n logn)}
+
+// Linear Search => The Linear Search algorithm searches through an array and returns the index of the value it searches for. {TimeComlexity 0(n)} 
+
+// Binary Search => The Binary Search algorithm searches through an array and returns the index of the value it searches for. {TimeComplexity O(log2n)}
