c73b97ae7e1ba2b456cf8b61b2636a62d6a2cdc9
husnain585 <husnain7t9@gmail.com>
2025-01-16 17:08:05 +0500

    Add PreparationFile

---
+++ interviewTasawar.js
@@ -0,0 +1,10 @@
+// Code DryRun => Wrong 
+// late binding vs early binding => ok 
+// design pattern => Worong
+// what is operator overloading => correct  
+// real life example of oop pillar => correct 
+// how can we call abstract class => almost ok
+// what is static method => ok 
+
+// ***************************************************
+
