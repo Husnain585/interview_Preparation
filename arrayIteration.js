3cbe88a05f25fc8ce026817d27d378c85fcc0e2d
husnain585 <husnain7t9@gmail.com>
2025-01-19 23:58:50 +0500

    Changing

---
+++ arrayIteration.js
@@ -0,0 +1,8 @@
+let array = [1,2,3,4];
+for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+        const element = array[j];
+        console.log(i * j);
+    }
+    
+}
