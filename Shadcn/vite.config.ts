4874f30617d396428c06adaea20189231a533a25
husnain585 <husnain7t9@gmail.com>
2025-01-20 18:44:10 +0500

    Shadcn setup config

---
+++ Shadcn/vite.config.ts
@@ -0,0 +1,13 @@
+import path = require("path")
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+})
+
